dc60cbd58a3ad72f8c57ae0a4f35340dd463599a
LibXZR <i@xzr.moe>
2022-06-29 23:03:35 +0800

    base: Introduce force background freezer
    This will freeze tasks with restricted battery usage when moving them
    to background and unfreeze them when moving to foreground. It's an
    unsafe freeze compared to the built-in cached app optimizer, which
    means victims may miss binder calls or even lead to crashes.
    
    But whatever, it is a real freezer, not a deal of "freeze or die".
    The latter one is what the built-in cached app optimizer is doing.
    Also, it responds much quicker than the built-in one and it freeze
    all backgrounds rather than cached apps only.
    
    Change-Id: I4caedca0aa234f2a402eb389c46e2142b5729772
    Signed-off-by: LibXZR <i@xzr.moe>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -8016,6 +8016,7 @@ public class ActivityManagerService extends IActivityManager.Stub
         mHiddenApiBlacklist.registerObserver();
         mSdkSandboxSettings.registerObserver();
         mPlatformCompat.registerContentObserver();
+        mOomAdjuster.registerContentObserver();
 
         mAppProfiler.retrieveSettings();
 
@@ -18449,11 +18450,6 @@ public class ActivityManagerService extends IActivityManager.Stub
         }
     }
 
-    @Override
-    public boolean isAppFreezerEnabled() {
-        return mOomAdjuster.mCachedAppOptimizer.useFreezer();
-    }
-
     /**
      * Resets the state of the {@link com.android.server.am.AppErrors} instance.
      * This is intended for testing within the CTS only and is protected by
@@ -18474,6 +18470,17 @@ public class ActivityManagerService extends IActivityManager.Stub
             return mOomAdjuster.mCachedAppOptimizer.enableFreezer(enable);
         } else {
             throw new SecurityException("Caller uid " + callerUid + " cannot set freezer state ");
+        }
+    }
+
+    @Override
+    public boolean isAppFreezerEnabled() {
+        final long token = Binder.clearCallingIdentity();
+
+        try {
+            return mOomAdjuster.mCachedAppOptimizer.useFreezer();
+        } finally {
+            Binder.restoreCallingIdentity(token);
         }
     }
 
@@ -18518,7 +18525,8 @@ public class ActivityManagerService extends IActivityManager.Stub
     }
     
     boolean shouldSkipBootCompletedBroadcastForPackage(ApplicationInfo info) {
-        return mActivityTaskManager.mAppStandbyInternal.isStrictStandbyPolicyEnabled() &&
+        return (mActivityTaskManager.mAppStandbyInternal.isStrictStandbyPolicyEnabled()
+                || mOomAdjuster.mForceBackgroundFreezerEnabled) &&
                 getAppOpsManager().checkOpNoThrow(
                         AppOpsManager.OP_RUN_ANY_IN_BACKGROUND,
                         info.uid, info.packageName) != AppOpsManager.MODE_ALLOWED;
